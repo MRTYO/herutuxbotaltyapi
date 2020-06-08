b4673836030574fadbaf91d1065797693729e6a5
MRTYO <63552299+MRTYO@users.noreply.github.com>
2020-06-08 19:38:24 +0300

    v12 icin
    discord.js v12 kurallları

--- komutlar/sev.js
+++ komutlar/sev.js
@@ -3,12 +3,12 @@ const Discord = require('discord.js');
 exports.run = (client, message, args) => {
 	let mesaj = args.slice(0).join(' ');
 	if (mesaj.length < 1) return message.reply('**Kimi Sevdiğini Yazmalısın  ? **');
-    const embed = new Discord.RichEmbed()
+    const embed = new Discord.MessageEmbed()
     .setAuthor('')
     .setColor("#36393F")
     .setDescription(`** ${mesaj} ` + message.author.username + ' Seni Çok Seviyor  :heart:**')
 	.setImage(`https://cdn.discordapp.com/attachments/534007653106581515/534420566573187072/tenor.gif`)
-    return message.channel.sendEmbed(embed);
+    return message.channel.send(embed);
 };
 
 exports.conf = {
